b74278a228c8dc25405db0e1ad9017a118c7cf4d
ssamarasam <samcsegct@gmail.com>
2023-04-28 20:26:22 +0530

    simple front end movie site using js

---
+++ script.js
@@ -0,0 +1,66 @@
+// console.log("Script is working");
+
+const API_KEY = `6834a9d0c7f832df978ea6b5d1be3457`;
+const APILINK = `https://api.themoviedb.org/3/discover/movie?
+sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+
+
+const main = document.getElementById('section');
+const form = document.getElementById('form');
+const search = document.getElementById('query');
+
+returnMovies(APILINK);
+function returnMovies(url) {
+    fetch(url).then(res => res.json())
+    .then(function(data) {
+        console.log(data.results);
+        data.results.forEach((element) => {
+            
+            const div_row = document.createElement('div');
+            div_row.setAttribute('class', 'row');
+
+            const div_column = document.createElement('div');
+            div_column.setAttribute('class', 'column')
+
+            const div_card = document.createElement('div');
+            div_card.setAttribute('class', 'card');
+
+            const image = document.createElement('img');
+            image.setAttribute('class', 'center');
+            image.setAttribute('class', 'thumbnail');
+            image.setAttribute('id', 'image');
+
+            const title = document.createElement('h3');
+            title.setAttribute('id', 'title');
+
+            title.innerHTML = `${element.title}`
+            image.src = IMG_PATH + element.poster_path;
+            div_card.appendChild(image);
+            div_card.appendChild(title);
+            div_column.appendChild(div_card);
+            div_row.appendChild(div_column);
+
+            main.appendChild(div_row);
+
+
+        })
+    })
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+
+    const searchItem = search.value;
+    if(searchItem) {
+        returnMovies(SEARCHAPI + searchItem);
+        search.value = '';
+    }
+})
+
+
+// 6834a9d0c7f832df978ea6b5d1be3457
+// https://api.themoviedb.org/3/movie/550?api_key=6834a9d0c7f832df978ea6b5d1be3457
+
